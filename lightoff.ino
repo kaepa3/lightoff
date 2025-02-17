b98d0ecc544ca474a80bf250a95728a237a24a8e
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2025-02-18 00:27:35 +0900

    Changes to be committed: 	modified:   .gitignore 	modified:   lightoff.ino

--- lightoff.ino
+++ lightoff.ino
@@ -1,11 +1,43 @@
-#include "config.h"
 
+class CheckPin {
+  public:
+    CheckPin(int pin);
+    bool IsOn();
+  private:
+    int _pin;
+    int _before;
+};
+
+CheckPin::CheckPin(int pin) {
+  _pin = pin;
+  pinMode(_pin, INPUT);
+}
+bool CheckPin::IsOn() {
+  bool val = digitalRead(_pin) == HIGH;
+  bool isChange = val != _before;
+  _before = val;
+  return val&&isChange;
+}
+
+CheckPin gpio(5);
+
+#define LED_BUILTIN 8
 void setup() {
-  
   Serial.begin(9600);
- Serial.println(OpenToken);
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("init end");
 }
 
 void loop() {
+  delay(1000);
+  if (gpio.IsOn()) {
+    Serial.println("GPIO 13 is on");
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else{
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("GPIO 13 is off");
+  }
 }
+
+
